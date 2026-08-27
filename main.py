e0f8f70b5e18b9a9ce5ddce6b05fb778bae4a292
Alex Dynamo <bloodchandelle100@gmail.com>
2026-08-27 15:18:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screenmanager import ScreenManager
 from kivymd.uix.filemanager import MDFileManager
 from kivymd.toast.kivytoast.kivytoast import toast
-from android.permissions import request_permissions, Permission
+import importlib
+import platform
 
-request_permissions([
-    Permission.READ_EXTERNAL_STORAGE,
-    Permission.WRITE_EXTERNAL_STORAGE
-])
+if platform.system() == 'Android':
+    permissions = importlib.import_module('android.permissions')
+    permissions.request_permissions([
+        permissions.Permission.READ_EXTERNAL_STORAGE,
+        permissions.Permission.WRITE_EXTERNAL_STORAGE
+    ])
 Window.keyboard_anim_args ={'d': .2, 't': 'in_out_expo'}
 Window.softinput_mode = "below_target"
 import os
